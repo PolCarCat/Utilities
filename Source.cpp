c3a746ce46c28dc995885547a5de686ee1250c03
PolCarCat <polcarcat@gmail.com>
2017-09-19 13:46:25 +0200

    Vec 3

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include"Vec3.h"
+
+int main()
+{
+
+
+		vec3<int> v1 = { 1,1,1 };
+		vec3<int> v2 = { 2,0,1 };
+
+		float v3 = v1.distance(v2);
+
+
+		return 0;
+}
---
+++ Vec3.h
@@ -0,0 +1,104 @@
+#pragma once
+#include <cmath>
+
+template <class T>
+
+class vec3
+{
+
+	T x, y, z;
+
+public:
+
+	vec3(T x1, T y1, T z1) {
+		x = x1;
+		y = y1;
+		z = z1;
+	}
+	~vec3(){
+	}
+
+
+	vec3 operator +(const vec3 vec)
+	{
+		vec3 ret
+		{
+		x + vec.x,
+		y + vec.y,
+		z + vec.z,
+		};
+		
+		return *ret;
+	}
+	vec3 operator -(const vec3 vec)
+	{
+		vec3 ret
+		{
+		x - vec.x,
+		y - vec.y,
+		z - vec.z
+		};
+		return ret;
+	}
+	vec3& operator +=(const vec3 vec)
+	{
+		x += vec.x;
+		y += vec.y;
+		z += vec.z;
+		return *this;
+
+	}
+	vec3& operator -=(const vec3 vec)
+	{
+		x -= vec.x;
+		y -= vec.y;
+		z -= vec.z;
+		return *this;
+	}
+	vec3& operator =(const vec3 vec)
+	{
+		x = vec.x;
+		y = vec.y;
+		z = vec.z;
+	}
+	bool operator == (const vec3 vec)
+	{
+		bool ret = false;
+		if (x == vec.x && y == vec.y && z = vec.z)
+		{
+			ret = true;
+		}
+		return ret;
+	}
+
+	vec3 Normalise() {
+		float module = sqrt(x*x + y*y + z * z);
+		vec3<float> aux
+		{
+		(float) x / module,
+		(float) y / module,
+		(float) z / module
+		}
+
+		return aux;
+	}
+	vec3 Zero() {
+		
+		vec3<float> aux{ 0,0,0 };
+
+		return aux;
+	}
+	bool isZero()
+	{
+		return this == {0, 0, 0};
+	}
+	float distance(const vec3 vec)
+	{
+		vec3 aux = *this - vec;
+		float module = sqrt(aux.x*aux.x + aux.y*aux.y + aux.z * aux.z);
+
+		return module;
+	}
+
+};
+
